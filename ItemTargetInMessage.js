7a3defc8c001af4dde00156d1643c60211eaa7cc
MihailJP <mihailjp@gmail.com>
2020-09-23 01:37:52 +0900

    Update ItemTargetInMessage.js
    There isn't message setting for items.

--- ItemTargetInMessage.js
+++ ItemTargetInMessage.js
@@ -1,41 +1,43 @@
 //=============================================================================
-// RPG Maker MZ - Include the target of item or skill in the message
+// RPG Maker MZ - Include the target of a skill in the message
 //=============================================================================
 
 /*:
  * @target MZ
- * @plugindesc Include the target of item or skill in the message
+ * @plugindesc Include the target of skill in the message
  * @author MihailJP
  * @url https://github.com/MihailJP/mihamzplugin/blob/master/ItemTargetInMessage.js
  * @orderAfter SpeakInBattle
  *
  * @help ItemTargetInMessage.js
  *
- * This plugin expands feature of the message shown when using items or skills.
+ * This plugin expands feature of the message shown when using skills.
  * Write %3 in the message to include the target's name.
  * If more than one target, the message could be redundant.
  *
  * It does not provide plugin commands.
  *
  * Changelog
+ * 23 Sept 2020: Update summary: There isn't message setting for items.
  * 23 Sept 2020: First edition.
  */
 
 /*:ja
  * @target MZ
- * @plugindesc アイテムやスキルのターゲットをメッセージに入れられるようにする
+ * @plugindesc スキルのターゲットをメッセージに入れられるようにする
  * @author MihailJP
  * @url https://github.com/MihailJP/mihamzplugin/blob/master/ItemTargetInMessage.js
  *
  * @help ItemTargetInMessage.js
  *
- * このプラグインは、スキルやアイテムを使用したときのメッセージを拡張します。
- * スキルやアイテムのメッセージに %3 と書くことで対象の名前を含めることができます。
+ * このプラグインは、スキルを使用したときのメッセージを拡張します。
+ * スキルのメッセージに %3 と書くことで対象の名前を含めることができます。
  * 対象が複数の場合はメッセージが冗長になります。
  *
  * プラグインコマンドはありません。
  *
  * 更新履歴
+ * 令和2年9月23日 説明を修正。アイテムにメッセージ設定はありません。
  * 令和2年9月23日 初版
  */
  
